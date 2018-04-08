dc6262680f1fca1d9e46fc38ba23423d5f6543db
Masatoshi Teruya <mah0x211@gmail.com>
2018-04-08 09:40:19 +0900

    Update eample/echoserver.lua

--- example/echoserver.lua
+++ example/echoserver.lua
@@ -21,183 +21,59 @@
   THE SOFTWARE.
 
   example/echoserver.lua
-  lua-synops
+  lua-act
 
   Created by Masatoshi Teruya on 16/12/24.
 
 --]]
-local inspect = require('util').inspect;
-local signal = require('signal')
+local Act = require('act');
+-- you need to install the net module as follows;
+--  $ luarocks install net
 local InetServer = require('net.stream.inet').server;
-local Synops = require('synops');
--- constants
-local HOST = '127.0.0.1';
-local PORT = '5000';
-local NPROC = tonumber(...);
-
--- check argument
-if type( NPROC ) ~= 'number' or NPROC < 0 then
-    NPROC = 1;
-elseif NPROC > 10 then
-    NPROC = 10;
-end
 
 
-local function send( sock, str, deadline )
-    local len, err, again = sock:send( str );
-
-    if not again then
-        return len, err;
-    else
-        local fd = sock:fd();
-        local total = 0;
-        local ok, timeout;
-
-        repeat
-            total = total + len;
-
-            if len > 0 then
-                str = str:sub( len + 1 );
-            end
-
-            ok, err, timeout = Synops.writable( fd, deadline );
-            if ok then
-                len, err, again = sock:send( str );
-            end
-        until not again or timeout == true;
-
-        return len and total + len, err, timeout;
-    end
-end
-
-
-local function recv( sock )
-    local msg, err, again = sock:recv();
-
-    if not again then
-        return msg, err;
-    else
-        local ok;
-
-        ok, err = Synops.readable( sock:fd() );
-        if ok then
-            return sock:recv();
-        end
-
-        return nil, err;
-    end
-end
-
-
-local function handleClient( client )
-    local err;
-
-    Synops.atexit( client.close, client );
-
+local function handler( client )
+    Act.atexit( client.close, client )
     while true do
-        local msg, ok;
+        local msg, err = client:recv()
 
-        msg, err = recv( client );
-        if not msg then break end
-
-        ok, err = send( client, msg );
-        if err then break end
-    end
-
-    if err then
-        print( err );
-    end
-end
-
-
-
-local function accept( sock )
-    local client, err, again = sock:accept();
-
-    if not again then
-        return client, err;
-    else
-        local ok;
-
-        repeat
-            ok, err = Synops.readable( sock:fd() );
-            if ok then
-                client, err, again = sock:accept();
-            else
-                print('Synops.readable() failure', ok, err, sock:fd())
-                return nil, err;
-            end
-        until not again;
-
-        return client, err;
-    end
-end
-
-
-local function handleServer( server )
-    Synops.atexit( server.close, server );
-
-    while true do
-        local client, err = accept( server );
-
-        -- got client
-        if client then
-            -- print( 'accept', client:fd() )
-            assert( Synops.spawn( handleClient, client ) );
-        else
-            print('failed to accept', err);
-            break;
-        end
-    end
-end
-
-
-local function worker( server )
-    err = server:listen( BACKLOG );
-    if err then
-        server:close();
-        error( err );
-    end
-
-    print( 'start worker: ', HOST, PORT, server:fd() );
-    -- spawn server handler
-    assert( Synops.spawn( handleServer, server ) );
-    print( 'end worker', Synops.sigwait( nil, signal.SIGUSR1 ) );
-end
-
-
-local function main()
-    -- create server
-    local server, err = InetServer.new({
-        host = HOST;
-        port = PORT,
-        reuseaddr = true,
-        nonblock = true,
-    });
-    local pids = {};
-    local pid;
-
-    if err then
-        error( err );
-    end
-
-    signal.blockAll();
-    for i = 1, NPROC do
-        pid, err = Synops.fork();
         if err then
-            print( err );
-        -- worker process
-        elseif pid == 0 then
-            return worker( server );
+            print('error:', err)
+            return
         end
 
-        pids[#pids + 1] = pid;
+        _, err = client:send(msg)
+        if err then
+            print('error:', err)
+            return
+        end
     end
-
-    -- start server
-    print( Synops.sigwait( nil, signal.SIGINT ) );
-    signal.killpg( 0, signal.SIGUSR1 );
-    print( 'end server' );
 end
 
-assert( Synops.run( main ) );
+
+Act.run(function()
+    local server, err = InetServer.new({
+        host = '127.0.0.1',
+        port = 5000,
+    });
+
+    err = err or server:listen()
+    if err then
+        error(err)
+    end
+
+    print( 'start server: ', '127.0.0.1', 5000 )
+    while true do
+        local client, err = server:accept()
+
+        if client then
+            Act.spawn(handler, client)
+        elseif err then
+            print('failed to accept', err)
+            break
+        end
+    end
+
+    server:close()
+    print( 'end server' )
+end)
