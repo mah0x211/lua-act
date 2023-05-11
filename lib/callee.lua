955c284c19c8a20fa7fec13c97c49ab7a495d686
Masatoshi Fukunaga <mah0x211@gmail.com>
2023-05-11 10:42:38 +0900

    Remove redundant code in Callee.sleep method

--- lib/callee.lua
+++ lib/callee.lua
@@ -599,18 +599,14 @@ function Callee:sleep(msec)
         return false, err
     end
 
-    -- revoke all events currently in use
-    self:revoke()
+    local cid = self.cid
     local deadline = getmsec() + msec
     -- wait until wake-up or resume by resume method
-    local cid = self.cid
     SUSPENDED[cid] = self
-    if yield() == OP_RUNQ then
+    local op = yield()
+    SUSPENDED[cid] = nil
+    if op == OP_RUNQ then
         local rem = deadline - getmsec()
-        if SUSPENDED[cid] then
-            -- resumed after sleep
-            SUSPENDED[cid] = nil
-        end
         return rem > 0 and rem or 0
     end
 
