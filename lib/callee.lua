fe82bf2825efe59eef09fe6b06bc6b1dd3ffceeb
Masatoshi Fukunaga <mah0x211@gmail.com>
2023-05-26 00:18:28 +0900

    Fix bug in writable function
    callee should be removed from RunQ if it is resumed via event.

--- lib/callee.lua
+++ lib/callee.lua
@@ -254,11 +254,14 @@ local function waitable(self, operators, asa, fd, msec)
     end
     event:revoke(ev)
 
+    -- timed out
     if self.op == OP_RUNQ then
         assert(msec ~= nil, 'invalid implements')
-        self.ctx:removeq(self)
-        -- timed out
         return false, nil, true
+    end
+
+    if msec then
+        self.ctx:removeq(self)
     end
 
     -- opertion type must be OP_EVENT
