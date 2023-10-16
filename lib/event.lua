0e66a2853d1809c47bb890e92d1c863481963fad
mah0x211 <mah0x211@gmail.com>
2023-10-17 01:28:51 +0900

    Fix bug that event.consume method did not consume all events

--- lib/event.lua
+++ lib/event.lua
@@ -221,22 +221,26 @@ function Event:consume(sec)
             -- got critical error
             return nil, new_errno(errno, err)
         elseif nev > 0 then
-            -- consuming events
-            local ev, evinfo, disabled = monitor:consume()
-
-            while ev do
-                -- resume
-                local callee = evinfo.callee
-                if callee then
-                    -- clear callee reference from event-info
-                    evinfo.callee = nil
-                    callee:call(OP_EVENT, evinfo.ev:ident(), disabled)
-                else
-                    -- set a flag to confirm that the event has already occurred
-                    evinfo.is_ready = true
+            for _ = 1, nev do
+                -- consuming events
+                local ev, evinfo, disabled = monitor:consume()
+                if ev then
+                    -- resume coroutine
+                    local callee = evinfo.callee
+                    if callee then
+                        -- clear callee reference from event-info
+                        evinfo.callee = nil
+                        callee:call(OP_EVENT, evinfo.ev:ident(), disabled)
+                    else
+                        -- set a flag to confirm that the event has already occurred
+                        evinfo.is_ready = true
+                    end
+                elseif not err and evinfo then
+                    -- if ev is nil
+                    --  * 2nd return value treated as error message
+                    --  * 3rd return value treated as errno
+                    err = new_errno(disabled, evinfo)
                 end
-                -- get next event
-                ev, evinfo, disabled = monitor:consume()
             end
         end
 
